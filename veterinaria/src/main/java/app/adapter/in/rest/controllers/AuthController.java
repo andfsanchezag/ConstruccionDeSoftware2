b236f1bfcf972c096c803d4f25ad4872aaa15289
andfsanchezag <andres.sanchez57@tdea.edu.co>
2025-10-16 16:06:13 -0500

    fix

--- veterinaria/src/main/java/app/adapter/in/rest/controllers/AuthController.java
+++ veterinaria/src/main/java/app/adapter/in/rest/controllers/AuthController.java
@@ -1,9 +1,12 @@
 package app.adapter.in.rest.controllers;
 
+import app.application.exceptions.BusinessException;
+import app.application.exceptions.InputsException;
 import app.domain.model.auth.AuthCredentials;
 import app.domain.model.auth.TokenResponse;
 import app.domain.services.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,8 +18,24 @@ public class AuthController {
     private AuthenticationService authenticationService;
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponse> login(@RequestBody AuthCredentials credentials) {
+    public ResponseEntity<?> login(@RequestBody AuthCredentials credentials) {
+    	try {
         TokenResponse response = authenticationService.authenticate(credentials);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(response);}
+    	catch (InputsException ie) {
+	        return ResponseEntity
+	                .status(HttpStatus.BAD_REQUEST)
+	                .body(ie.getMessage());
+
+	    } catch (BusinessException be) {
+	        return ResponseEntity
+	                .status(HttpStatus.UNAUTHORIZED)
+	                .body(be.getMessage());
+
+	    } catch (Exception e) {
+	        return ResponseEntity
+	                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+	                .body(e.getMessage());
+	    }
     }
 }
